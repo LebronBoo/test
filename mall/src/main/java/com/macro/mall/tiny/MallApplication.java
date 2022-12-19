c21b19b59768a227dc620a44a262b9995161bcc8
wanghoubo111 <727546192@qq.com>
2022-12-19 21:04:05 +0800

    pom

--- mall/src/main/java/com/macro/mall/tiny/MallApplication.java
+++ mall/src/main/java/com/macro/mall/tiny/MallApplication.java
@@ -1,4 +1,4 @@
-package com.example.mall;
+package com.macro.mall.tiny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- mall/src/test/java/com/macro/mall/tiny/MallApplicationTests.java
+++ mall/src/test/java/com/macro/mall/tiny/MallApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mall;
+package com.macro.mall.tiny;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

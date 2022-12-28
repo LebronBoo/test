47738a7be08e67d78bc570c43a217083bc6d059b
wanghoubo111 <727546192@qq.com>
2022-12-28 22:57:52 +0800

    generate test

---
+++ mall/src/main/java/com/example/mall/mbg/model/User.java
@@ -0,0 +1,74 @@
+package com.example.mall.mbg.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private String id;
+
+    private String username;
+
+    private String pw;
+
+    private Date createdate;
+
+    private String age;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", pw=").append(pw);
+        sb.append(", createdate=").append(createdate);
+        sb.append(", age=").append(age);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
